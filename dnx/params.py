a5864da9a5dbdaa2983d641cee8918b883899564
AHReccese <amirhssin6rst@gmail.com>
2026-04-01 18:46:26 -0400

    Add DNS preset for AdGuard and set default ping parameters

--- dnx/params.py
+++ dnx/params.py
@@ -8,7 +8,10 @@ DNS_PRESETS = {
     "cloudflare": ["1.1.1.1", "1.0.0.1"],
     "quad9": ["9.9.9.9", "149.112.112.112"],
     "opendns": ["208.67.222.222", "208.67.220.220"],
+    "adguard": ["94.140.14.14", "94.140.15.15"],
 }
 
 LINUX_RESOLV_CONF = "/etc/resolv.conf"
 
+DEFAULT_PING_COUNT = 3
+DEFAULT_PING_TIMEOUT = 5
